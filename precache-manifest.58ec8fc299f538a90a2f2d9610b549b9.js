c9b9cad48145aa4658dce254f3d7ddf2e83a757d
Andrei Aliaksandrau <57172661+IAleks94@users.noreply.github.com>
2020-05-23 19:10:44 +0300

    feat: remuve files to deploy

---
+++ precache-manifest.58ec8fc299f538a90a2f2d9610b549b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541422451ae2df19407ca47f10b3b5a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df6ea84cadad1161be8b",
+    "url": "/static/css/main.85ed4417.chunk.css"
+  },
+  {
+    "revision": "e0bc101c4f46b9b88a61",
+    "url": "/static/js/2.6fa9c5f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6fa9c5f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6ea84cadad1161be8b",
+    "url": "/static/js/main.a14ae42a.chunk.js"
+  },
+  {
+    "revision": "7b84cece31479a82f2d3",
+    "url": "/static/js/runtime-main.16b261c7.js"
+  },
+  {
+    "revision": "13df8a3d0feffbd05df8020a77e9c234",
+    "url": "/static/media/bg3.13df8a3d.png"
+  },
+  {
+    "revision": "6cc46d552ce45467ef9e787ff72c2e18",
+    "url": "/static/media/microphone.6cc46d55.svg"
+  },
+  {
+    "revision": "be8d82ec53ba27999563f8e497d8e7c6",
+    "url": "/static/media/refreshBG.be8d82ec.svg"
+  }
+]);
